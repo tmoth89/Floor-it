6bf34cd635a34a94e2d86bece3d120b50ccc8e7c
Noah <timsort101@gmail.com>
2019-06-12 11:41:56 -0700

    Login Done.

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,24 +1,31 @@
 import React from 'react';
+import { genericTypeAnnotation } from '@babel/types';
 class SignIn extends React.Component{
- 
     constructor(props){
         super(props)
         this.state = {};
     }
 
+
    render(){
-       let {email, password} = this.state;
+       const styles = {
+        width: '200px',
+        height: '100px',
+        padding: '20px',
+        margin: '100px auto',
+        boxShadow: '0px 0px 10px 2px grey'
+       }
+
        return (
-       <div class='login-wrap'>
-          <form name='loginForm'>
+        <div className='login-wrap' style={styles}>
+         
             <label>Name</label>
             <input type='name'name='email'/>
-            <br/>
+         
             <label>Password</label>
             <input type='password' name='password'/>
             <input type='submit' value='login'/>
-          </form>
-       </div>
+        </div>
        );
    }
 }
